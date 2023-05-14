f571a77958be0f479c6d2f3a5935c8460c38fd48
Mikael Klevengård <mickekleven@outlook.com>
2023-05-14 21:47:59 +0200

    cfd

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,1 +1,20 @@
 const skillId = document.querySelectorAll("cu-badge");
+
+const languages = ["C#", "Razor Syntax", "Js[Isolation]", "Sass", "Css", "Html", "Sql"];
+
+// Set lang information text to target element
+const langPresFunc = () => {
+  let _langPres = document.querySelector("#presLang");
+
+  _langPres.html = "";
+
+  let html = languages.map((x) => `<li>${x}</li>`).join("");
+
+  console.log(html);
+
+  if (_langPres !== null) {
+    _langPres.insertAdjacentHTML("afterbegin", html);
+  }
+};
+
+langPresFunc();
