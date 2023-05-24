7d29a2f344ebee64d9479cdf676b051195dfe09b
Mikael Klevengård <mickekleven@outlook.com>
2023-05-24 12:46:03 +0200

    Add quicklink btn

---
+++ assets/js/quicklinksrc.js
@@ -0,0 +1,38 @@
+let parentAnchor = document.getElementById("parentAnchor");
+let anchorLink = document.querySelector("#anchorElement");
+
+let linkArray = [
+  { anchorTxt: "Babaazoit project", link: "babaazoitproject.html" },
+  { anchorTxt: "Personal project", link: "blzwasmproject.html" },
+  { anchorTxt: "About me", link: "me.html" },
+  { anchorTxt: "Skills", link: "skills.html" },
+];
+
+async function sleep(time) {
+  return new Promise((resolve, _) => setTimeout(resolve, time));
+}
+
+async function scroller() {
+  for (let i = 0; i < linkArray.length; i++) {
+    setElement(linkArray[i]);
+
+    if (i === linkArray.length - 1) {
+      i = 0;
+    }
+
+    await sleep(5000);
+  }
+
+  //result.style.opacity = 0.2;
+  await sleep(1000);
+  //result.style.display = "none";
+}
+
+const setElement = (content) => {
+  if (anchorLink === null) return;
+
+  anchorLink.href = content.link;
+  anchorLink.innerHTML = content.anchorTxt;
+};
+
+scroller();
